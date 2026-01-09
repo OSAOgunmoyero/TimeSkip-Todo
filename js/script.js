4a7822b9e6800e1423171d14db37670509f33104
OSAOgunmoyero <seun.ogunmoyero@gmail.com>
2026-01-09 00:27:04 -0500

    Changing up landing page, added sections, slight animation

--- js/script.js
+++ js/script.js
@@ -1,13 +1,18 @@
-// Getting our DOM elements
+// Getting our DOM element variables
 const addTaskBtn = document.querySelector('.addTaskBtn');
 const modal = document.querySelector('#addTaskModal');
-
+const scrollArrow  = document.querySelector('.scrollArrow');
+const taskArea = document.querySelector('#task-section');
 
 
 
 
 //Methods
+
+
+
 addTaskBtn.addEventListener('click', function () {
   modal.showModal();
+  // document.body.classList.add('modal-open');
 });
 
