06cad0ac7207098f5ab4599cda834f9702d65de4
OSAOgunmoyero <seun.ogunmoyero@gmail.com>
2026-01-10 23:31:07 -0500

    Working on modal. Added Event Listeners, changed sizes, working on responsiveness

--- js/script.js
+++ js/script.js
@@ -1,18 +1,23 @@
 // Getting our DOM element variables
 const addTaskBtn = document.querySelector('.addTaskBtn');
 const modal = document.querySelector('#addTaskModal');
-const scrollArrow  = document.querySelector('.scrollArrow');
-const taskArea = document.querySelector('#task-section');
+const newTaskInput = document.querySelector('#task-name');
+const newTaskDescription = document.querySelector('#task-details');
+const taskModalXBtn = document.querySelector('.close-modal-btn');
 
 
 
+//Event Listeners
 
-//Methods
-
-
-
+// For Modal
 addTaskBtn.addEventListener('click', function () {
+  newTaskInput.value = '';
+  newTaskDescription.value = '';
   modal.showModal();
-  // document.body.classList.add('modal-open');
+  document.body.classList.add('modal-open');
 });
 
+taskModalXBtn.addEventListener('click', function () {
+  modal.close();
+  document.body.classList.remove('modal-open');
+})
